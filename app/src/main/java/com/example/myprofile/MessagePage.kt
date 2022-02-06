10bf962c45c3500c65ba58c5a374d100c0426d2f
DannyMtwenty <dannywanyoike@gmail.com>
2022-02-06 23:04:53 +0300

    message textField and Navigation components

--- app/src/main/java/com/example/myprofile/MessagePage.kt
+++ app/src/main/java/com/example/myprofile/MessagePage.kt
@@ -1,13 +1,17 @@
 package com.example.myprofile
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +24,13 @@ fun messagePage(){
     var name by rememberSaveable() {   //rememberSaveable saves value when configuration changes e.g screen rotation
         mutableStateOf("")
     }
-
+    Card(
+        modifier = Modifier
+            .padding(top = 100.dp, bottom = 100.dp, start = 20.dp, end = 20.dp)
+            .fillMaxSize()
+            .border(width = 3.dp, color = Color.White, shape = RoundedCornerShape(30.dp)),
+        elevation = 7.dp //card shadow effect
+    ) {
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement= Arrangement.Center,
     modifier = Modifier
@@ -38,6 +48,7 @@ fun messagePage(){
          Text(text = "send")
         }
 
+    }
     }
 }
 
